6f18b7be69a2e4a4261dfed8560256916035842b
Richard Stiehm <richardstiehm@gmail.com>
2016-02-24 12:31:00 -0500

    Update PORT FOR HEROKU

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var express = require("express");
 var app = express();
-var PORT = 3000;
-
-
+var PORT = process.env.PORT || 3000;
 var middleware = require("./middleware.js")
 
 
@@ -14,12 +12,14 @@ app.use(middleware.logger);
 
 //ROUTES ----------------------------------------------------------
 app.get('/about', middleware.requireAuthentication, function(req, res, next){
-	res.send("This is the About Route");
+	res.send("<h1>This is the About Route</h1>");
 });
 
 
 
-app.use(express.static(__dirname + '/public')); //LINKS PUBLIC DIR
+
+
+app.use(express.static(__dirname + '/public')); //LINKS PUBLIC DIRECTORY 
 
 app.listen(PORT, function() {
 	console.log("Express is running on PORT " + PORT);
